4bcfb59bf74806e3c5b4ab6e25d8263e8ee641cc
Bogdan Floris <bogdan.floris@gmail.com>
2023-12-01 13:39:16 +0200

    feat: day 1 part 2

--- src/day_1/day_1.zig
+++ src/day_1/day_1.zig
@@ -3,9 +3,48 @@ const parseInt = std.fmt.parseInt;
 const Part = @import("utils").Part;
 const expect = std.testing.expect;
 
+const String = []const u8;
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
+
+const RepMap = struct {
+    letters: []const u8,
+    digit: u8,
+    len: usize,
+};
+const one = RepMap{ .letters = "one", .digit = '1', .len = 3 };
+const two = RepMap{ .letters = "two", .digit = '2', .len = 3 };
+const three = RepMap{ .letters = "three", .digit = '3', .len = 5 };
+const four = RepMap{ .letters = "four", .digit = '4', .len = 4 };
+const five = RepMap{ .letters = "five", .digit = '5', .len = 4 };
+const six = RepMap{ .letters = "six", .digit = '6', .len = 3 };
+const seven = RepMap{ .letters = "seven", .digit = '7', .len = 5 };
+const eight = RepMap{ .letters = "eight", .digit = '8', .len = 5 };
+const nine = RepMap{ .letters = "nine", .digit = '9', .len = 4 };
+const numbers = [_]RepMap{ one, two, three, four, five, six, seven, eight, nine };
+
 fn getCalibrationWithLetters(line: []const u8) !u64 {
-    _ = line;
-    return 0;
+    var first: ?u8 = null;
+    var last: u8 = undefined;
+    for (line, 0..) |char, i| {
+        for (numbers) |number| {
+            if (i + number.len <= line.len and std.mem.eql(u8, number.letters, line[i .. i + number.len])) {
+                if (first == null) {
+                    first = number.digit;
+                }
+                last = number.digit;
+            }
+        }
+        if (char >= '0' and char <= '9') {
+            if (first == null) {
+                first = char;
+            }
+            last = char;
+        }
+    }
+    const calibration = [_]u8{ first.?, last };
+    return try parseInt(u64, &calibration, 10);
 }
 
 fn getCalibration(line: []const u8) !u64 {
@@ -47,9 +86,28 @@ pub fn solve() !void {
 }
 
 ////// TESTS //////
-const test_input_1 = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n";
+const test_input_1 =
+    \\1abc2
+    \\pqr3stu8vwx
+    \\a1b2c3d4e5f
+    \\treb7uchet
+;
+const test_input_2 =
+    \\two1nine
+    \\eightwothree
+    \\abcone2threexyz
+    \\xtwone3four
+    \\4nineeightseven2
+    \\zoneight234
+    \\7pqrstsixteen
+;
 
 test "part 1" {
     const result = process(test_input_1, Part.one);
     try expect(result == 142);
+}
+
+test "part 2" {
+    const result = process(test_input_2, Part.two);
+    try expect(result == 281);
 }
